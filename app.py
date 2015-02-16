f96762a75ce036bbc42543e736fb934a4bbcd0f0
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-02-16 15:31:44 -0800

    Base code

---
+++ app.py
@@ -0,0 +1,11 @@
+import twitter 
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET =''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+
+auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                           CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter_api = twitter.Twitter(auth=auth)
