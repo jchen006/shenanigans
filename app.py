2a5316b6d7b1c65b594484a95718105ac2d8dbce
Siddhartho Bhattacharya <sid_gogol@hotmail.com>
2015-02-16 16:02:29 -0800

    argparse to main

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import twitter 
+import twitter, argparse
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET =''
@@ -12,6 +12,25 @@ auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
 twitter_api = twitter.Twitter(auth=auth)
 
 
-def main(): 
+if __name__=='__main__':
+    desc = "App to create a map of tweets from Berkeley"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('secrets')
+    args = parser.parse_args()
 
+    with open(args.secrets, 'r') as file:
+        lines = file.readlines()
 
+    for line in lines[:-1]:
+        val = line.split(', ')[1][:-1]
+        if line.startswith('Consumer Key'):
+            CONSUMER_KEY = val
+        elif line.startswith('Consumer Secret'):
+            CONSUMER_SECRET = val
+        elif line.startswith('Access Token'):
+            OAUTH_TOKEN = val
+        elif line.startswith('Token Secret'):
+            OAUTH_TOKEN_SECRET = val
+    
+
+            
