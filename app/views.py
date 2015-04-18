911557e3b72b89fb413aa7d1ac945d381f3a8f73
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-04-18 15:18:44 -0700

    udpated pages

--- app/views.py
+++ app/views.py
@@ -2,7 +2,9 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html',
-                           title='Home')
+def home():
+  return render_template('home.html')
+  
+@app.route('/about')
+def about():
+  return render_template('about.html')
