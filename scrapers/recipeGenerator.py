e0acbe69282d9fa9b1d6d5ac4cb2b9ecdf20c920
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-04-29 19:34:16 -0700

    Changed naming

--- scrapers/recipeGenerator.py
+++ scrapers/recipeGenerator.py
@@ -8,17 +8,24 @@ class Recipe_Generator:
 		self.file_name = title + ".txt"
 		return self.generate_path() + self.file_name
 
-	def write_to_text(self, name, recipe_url, recipe_ingred, recipe_instruct): 
+	def write_to_text(self, name, recipe_url, recipe_ingred, recipe_instruct, chef=None): 
+		print "Writing to a text file"
 		file_name = self.generate_file_name(name)
 		text_file = open(file_name, "w")
 		text_file.write(name + "\n")
 		text_file.write(recipe_url + "\n")
+		if chef is not None: 
+			text_file.write(chef + "\n")
+
 		text_file.write("Ingredients \n")
+	
 		for i in recipe_ingred: 
 			text_file.write(i + "\n")
+
 		text_file.write("Instructions \n")
 		for i in recipe_instruct: 
 			text_file.write(i + "\n")
+
 		text_file.close()
 
 	def generate_path(self):
