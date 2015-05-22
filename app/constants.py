8d75b0ecd73c442542d296fe7ce5b7c476f7f344
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-05-22 12:02:14 -0700

    Added constants

---
+++ app/constants.py
@@ -0,0 +1,8 @@
+measurements = ['cups', 'oz', 'tsp', 'tablespoons', 
+	'teaspoons', 'tabelspoon', 'teaspoon', 'tbsp', 'cup', 'fl', 
+	'g', 'bunch', 'handful', 'pinch', "piece of", 'bunches', 'handful', 
+	'sprig', 'heads']
+
+conjunctions = ['and','but','or','yet','for','nor','so']
+
+size = ['small', 'medium', 'large']
