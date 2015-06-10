ba6532ac7a04fd72786604af48905bf0968d01ae
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-06-09 16:29:59 -1000

    Added a unpickle file
    name is used as the key to dictionary to the data of the recipe

--- app/parser.py
+++ app/parser.py
@@ -37,7 +37,7 @@ class parser:
 		file_path = get_parent_dir(os.getcwd()) + "/" + directory + "/"
 		return file_path
 
-	def pickling(self): 
+	def pickle(self): 
 		print "Picking and pickling"
 		path = self.generate_path("recipes")
 		for i in os.listdir(path):
@@ -48,9 +48,13 @@ class parser:
 		with open(self.generate_path("data") + 'recipes.pickle', 'w') as handle:
  			pickle.dump(self.recipes, handle)
 
+ 	def unpickle(self): 
+ 		self.recipes = pickle.load(open(self.generate_path("data") + 'recipes.pickle', "r" ))
+
 def main(): 
 	p = parser()
-	p.pickling()
+	p.pickle()
+	# p.unpickle()
 
 if __name__=="__main__": 
 	main()
