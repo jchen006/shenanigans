591f08fbd4b4876cfe8716851aef18090a199a5d
Siddhartho Bhattacharya <sid_gogol@hotmail.com>
2015-06-09 17:22:04 -0700

    added graph generation from a pickle file UNTESTED - requires modification of Data namedtuple to include name field

--- app/graph.py
+++ app/graph.py
@@ -1,10 +1,28 @@
+import collections as c
+import pickle, os
+
+Data = c.namedtuple("Data", "name url chef ingredients")
 
 class Graph: 
 
 	def __init__(self): 
 		self.graph = {}
+        self.num_nodes = 0
 
-	def generate_graph(self):
-		pass
+    def add_node(self, data_obj):
+        for i in data_obj.ingredients:
+            if self.graph[i] is None:
+                self.graph[i] = []
+            else:
+                self.graph[i].append(data_obj.name)
+        pass
+
+	def make_graph_from_pickle(self, filename):
+        with open(filename, 'r') as f:
+            recipes = pickle.load(f)
+        for d in [recipes[k] for k in recipes.keys()]:
+            add_node(d)
+        return 
+
 
 
