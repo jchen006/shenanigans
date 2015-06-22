1ef9f977fe26f87a8d0a72906103193a2da219cc
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-06-22 22:15:38 +0800

    deleted numpy redeploy

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from app import app
-import numpy as np
 from flask import render_template
 import json
 
@@ -14,21 +13,3 @@ def about():
 @app.route('/d3')
 def d3(): 
 	return render_template('d3.html')
-
-@app.route("/data")
-@app.route("/data/<int:ndata>")
-def data(ndata=100):
-    """
-    On request, this returns a list of ``ndata`` randomly made data points.
-    :param ndata: (optional)
-        The number of data points to return.
-    :returns data:
-        A JSON string of ``ndata`` data points.
-    """
-    x = 10 * np.random.rand(ndata) - 5
-    y = 0.5 * x + 0.5 * np.random.randn(ndata)
-    A = 10. ** np.random.rand(ndata)
-    c = np.random.rand(ndata)
-    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i],
-        "color": c[i]}
-        for i in range(ndata)])
