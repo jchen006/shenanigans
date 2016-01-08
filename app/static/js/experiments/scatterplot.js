d9166cd52c76ae31ad359295675f1ca23cce174e
Jeffrey H. Chen <jeffreyhc@berkeley.edu>
2016-01-07 21:45:26 -0800

    changed calls

--- app/static/js/experiments/scatterplot.js
+++ app/static/js/experiments/scatterplot.js
@@ -18,7 +18,7 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#scatterplot").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
