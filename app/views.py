e90b8d3094d193075daa30841cd7a5cd67c0ee80
Jeffrey H. Chen <jeffreyhc@berkeley.edu>
2016-01-07 21:44:27 -0800

    added scatterplot files

--- app/views.py
+++ app/views.py
@@ -95,27 +95,9 @@ def lda_graph_json():
 def lda_graph(): 
     return render_template('lda_graph.html')
 
-@app.route('/d3')
-def d3(): 
-    return render_template('d3.html')
-
-@app.route("/data")
-@app.route("/data/<int:ndata>")
-def data(ndata=100):
-    """
-    On request, this returns a list of ``ndata`` randomly made data points.
-    :param ndata: (optional)
-        The number of data points to return.
-    :returns data:
-        A JSON string of ``ndata`` data points.
-    """
-    x = 10 * np.random.rand(ndata) - 5
-    y = 0.5 * x + 0.5 * np.random.randn(ndata)
-    A = 10. ** np.random.rand(ndata)
-    c = np.random.rand(ndata)
-    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i],
-        "color": c[i]}
-        for i in range(ndata)])
+@app.route('/recipe_scatterplot')
+def recipe_scatterplot(): 
+    return render_template('recipe_scatterplot.html')
 
 @app.route("/mongo")
 def readMongo():
