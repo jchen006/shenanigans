61ca8d25cfe94d152125dba9725dd29761273ac4
Jeffrey H. Chen <jeffreyhc@berkeley.edu>
2016-06-22 22:36:40 -0700

    added food network

--- app/static/js/sandbox/food_network.js
+++ app/static/js/sandbox/food_network.js
@@ -64,63 +64,73 @@ var link = container.append("g")
     .attr("class", "link")
     .style("stroke-width", function(d) { return Math.sqrt(d.value); });
  
-        var node = container.append("g")
-            .attr("class", "nodes")
-            .selectAll(".node")
-            .data(graph.nodes)
-            .enter().append("g")
-            .attr("class", "node")
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+var node = container.append("g")
+    .attr("class", "nodes")
+    .selectAll(".node")
+    .data(graph.nodes)
+    .enter().append("g")
+    .attr("class", "node")
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; });
           
-        node.append("circle")
-            .attr("r", function(d) { return d.weight * 2+ 12; });
-         
+node.append("circle")
+    .attr("r", function(d) { 
+        console.log(d.name + " " + d.weight);
+        return d.weight * 2+ 12; 
+    })
+    .attr("fill", "white")
+    .attr("stroke", "black");
+
+force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("transform", function(d) { 
+        return "translate(" + d.x + "," + d.y + ")"; });
+    });
                 
-                force.on("tick", function() {
-                    link.attr("x1", function(d) { return d.source.x; })
-                        .attr("y1", function(d) { return d.source.y; })
-                        .attr("x2", function(d) { return d.target.x; })
-                        .attr("y2", function(d) { return d.target.y; });
+var linkedByIndex = {};
 
-                    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-                });
-                
-                var linkedByIndex = {};
-                graph.links.forEach(function(d) {
-                    linkedByIndex[d.source.index + "," + d.target.index] = 1;
-                });
+graph.links.forEach(function(d) {
+    linkedByIndex[d.source.index + "," + d.target.index] = 1;
+});
 
-                function isConnected(a, b) {
-                    return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index];
-                }
+function isConnected(a, b) {
+    return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index];
+}
 
-        node.on("mouseover", function(d){
-            node.classed("node-active", function(o) {
-                thisOpacity = isConnected(d, o) ? true : false;
-                this.setAttribute('fill-opacity', thisOpacity);
-                return thisOpacity;
-            });
+node.on("mouseover", function(d){
+    node.classed("node-active", function(o) {
+        thisOpacity = isConnected(d, o) ? true : false;
+        this.setAttribute('fill-opacity', thisOpacity);
+        return thisOpacity;
+    });
 
-            link.classed("link-active", function(o) {
-                return o.source === d || o.target === d ? true : false;
-            });
+    link.classed("link-active", function(o) {
+        return o.source === d || o.target === d ? true : false;
+    });
                         
-            d3.select(this).classed("node-active", true);
-            d3.select(this).select("circle").transition()
-                .duration(750)
-                .attr("r", (d.weight * 2+ 12)*1.5);
-            })
-        
-        .on("mouseout", function(d){
-                        
-                        node.classed("node-active", false);
-                        link.classed("link-active", false);
+    d3.select(this).classed("node-active", true);
+
+    d3.select(this).select("circle").transition()
+        .duration(750)
+        .attr("r", (d.weight * 2+ 12)*1.5);
+    })
+    .on("mouseout", function(d){
+        node.classed("node-active", false);
+        link.classed("link-active", false);
                     
-                        d3.select(this).select("circle").transition()
-                                .duration(750)
-                                .attr("r", d.weight * 2+ 12);
-                });
+        d3.select(this).select("circle").transition()
+            .duration(750)
+            .attr("r", d.weight * 2+ 12);
+    });
+
+node.append("text")
+    .attr("dy", ".35em")
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d.name; });
 
 
 function dottype(d) {
