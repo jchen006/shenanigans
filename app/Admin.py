5a57a783bb50285b525ad4a517935637b5c6bacc
Ryan Hang <ryanhang92@gmail.com>
2016-08-31 23:22:42 -0700

    lasted update on findUser

--- app/Admin.py
+++ app/Admin.py
@@ -4,24 +4,32 @@ admin  = Blueprint('admin', __name__)
 import mongo_admin_helper as mah
 from flask.ext.login import UserMixin, login_required
 
+#Ah! My issues is that i need to instant the admin helper object 
+#I need to call it on the the class of the file
+mongo_admin = mah.AdminMongoHelper('admin')
+
 @admin.route('/admin_signin')
 def admin_form():
   return render_template('admin/admin_signin.html')
 
 @lm.user_loader
 def user_loader(user_id):
-  return mah.getUser(user_id)
+  return mongo_admin.getUser(user_id)
 
 @admin.route('/admin_login', methods=['GET', 'POST'])
 def login():
-  print "login called", request, request.form, request.form.keys()
+  print "login logged request", request, request.form, request.form.keys()
   formUser = request.form['userId']
   formPassword = request.form['password']
-  admin = mah.getUser(formUser)
+  print "before db lookup"
+  admin = mongo_admin.getUser(formUser)
+  print "after db lookup"
   if admin:
+    print "admin is found"
     if password == admin.password:
       login_user(admin, remember=True)
       return redirect(url_for("admin.control_panel")) #Flash comment
+    print "admin is not found"
   return render_template("admin/admin_login.html")
 
 @admin.route('/admin_logout', methods=['GET', 'POST'])
--- app/mongo_admin_helper.py
+++ app/mongo_admin_helper.py
@@ -4,11 +4,18 @@ from bson.objectid import ObjectId
 import json
 
 class AdminMongoHelper:
-
+	#uri = "mongodb://recipe_user:dinneriscoming@ds035543.mongolab.com:35543/recipes"	
+	uri = "mongodb://ohtrahddis:dinneriscoming1@ds035543.mlab.com:35543/recipes"	
 	def __init__(self, collection, debug=False):
 		self.client = MongoClient()
 		self.db = self.client['admin']
 		self.collection = self.db[collection]
 
 	def getUser(self, userId):
-		return self.collection.find_one({"userId": userId})
+		print "getUser is called"
+		userObject = self.collection.find_one({"userId": userId})
+		if userObject:
+			"find_one returns something"
+			return userObject
+		"nothing is found"
+		return userObject
