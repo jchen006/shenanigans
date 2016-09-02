9bc55ab77cb5d64bafbf1729cc3ee20b243c0039
Siddhartho Bhattacharya <Siddhartho Bhattacharya>
2016-09-01 22:54:58 -0700

    tabbing

--- app/mongo_bayes_helper.py
+++ app/mongo_bayes_helper.py
@@ -3,23 +3,23 @@ import json
 import cPickle as pickle 
 
 class MongoBayesHelper: 
-	uri = "mongodb://recipe_user:dinneriscoming@ds017736.mlab.com:17736/bayesian_training"
+    uri = "mongodb://recipe_user:dinneriscoming@ds017736.mlab.com:17736/bayesian_training"
 
-	def __init__(self, debug=False): 
-		self.client = MongoClient(MongoBayesHelper.uri)
-		self.db = self.client['bayesian_training']
-		self.collection = self.db['training_data']
+    def __init__(self, debug=False): 
+        self.client = MongoClient(MongoBayesHelper.uri)
+        self.db = self.client['bayesian_training']
+        self.collection = self.db['training_data']
 
-	def insertToRemote(self, json):
-	    post_id = None
-	    if isinstance(json, dict):
-	        post_id = [self.collection.insert_one(json).inserted_id]
-	    elif isinstance(json, list):
-	        post_id = self.collection.insert_many(json).inserted_ids
-	    return post_id
+    def insertToRemote(self, json):
+        post_id = None
+        if isinstance(json, dict):
+            post_id = [self.collection.insert_one(json).inserted_id]
+        elif isinstance(json, list):
+            post_id = self.collection.insert_many(json).inserted_ids
+        return post_id
 
-	def findAll(self):
-		posts = []
-		for post in self.collection.find():
-			posts.append(post)
-		return posts
+    def findAll(self):
+        posts = []
+        for post in self.collection.find():
+            posts.append(post)
+        return posts
