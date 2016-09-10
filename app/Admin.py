712685d1d16326d93743546076c73d4119a2b69d
Jeffrey H. Chen <jeffreyhc@berkeley.edu>
2016-09-10 10:50:32 -0700

    AdminPanel (#59)
    * added admin panel pending item rendering
    
    * added login page for admin
    
    * cleaning up admin directions
    
    * Added mongo routes
    
    * Fixed issues and still debugging POST routes
    
    * Removing works
    
    * Added a mongo route check
    
    * adding login logic
    
    * added 200 and ITEM_NOT_FOUND
    
    * Fixed submission bug
    
    * mongo post request to approve is working
    
    * added check
    
    * fixed form 405 bug, form sends request to login, need to fix admin mongo getUser method
    
    * need to check if update_pending works
    
    * working on bayes filter
    
    * fixed bracket bug on line 56 of mongo submit helper
    
    * lasted update on findUser
    
    * working on bayes filter helper...
    
    * fixed mongo error, need to modify flask object next
    
    * issue with connecting driver
    
    * tabbing
    
    * login to admin panel is successful
    
    * logout functionality complete

--- app/Admin.py
+++ app/Admin.py
@@ -1,12 +1,14 @@
 from app import lm
-from flask import Blueprint, render_template, request #I did not import the request library
+from flask import Blueprint, render_template, request, redirect, url_for #I did not import the request library
 admin  = Blueprint('admin', __name__)
 import mongo_admin_helper as mah
-from flask.ext.login import UserMixin, login_required, login_user
+import mongo_helper as mh
+from flask.ext.login import UserMixin, login_required, login_user, logout_user, UserMixin
 
 #Ah! My issues is that i need to instant the admin helper object 
 #I need to call it on the the class of the file
 mongo_admin = mah.AdminMongoHelper('admin')
+mongo_recipe = mh.MongoHelper()
 
 @admin.route('/admin_signin')
 def admin_form():
@@ -14,7 +16,7 @@ def admin_form():
 
 @lm.user_loader
 def user_loader(user_id):
-  return mongo_admin.getUser(user_id)
+  return User(mongo_admin.getUser(user_id)["userId"])
 
 @admin.route('/admin_login', methods=['GET', 'POST'])
 def login():
@@ -28,15 +30,15 @@ def login():
     if formPassword == admin[formPassword]:
       #Almost there, the ADMIN object in the DB must have those 
       #Attrbutes, but most of this works
-
-      login_user(admin, remember=True)
-      return redirect(url_for("admin.control_panel.html")) #Flash comment
+      adminUser = User(admin["userId"])
+      login_user(adminUser, remember=True)
+      return redirect(url_for("admin.control_panel")) #Flash comment
   return render_template("admin/admin_signin.html")
 
 @admin.route('/admin_logout', methods=['GET', 'POST'])
 def logout():
   logout_user()
-  return render_template("admin_signin.html")
+  return render_template("admin/admin_signin.html")
 
 @admin.route('/control_panel')
 @login_required
@@ -46,3 +48,25 @@ def control_panel():
 	return render_template('admin/control_panel.html', pendingItems=pendingItems)
 
 #4) Script to create admin user
+
+
+class User(UserMixin):
+
+  def __init__(self, username):
+      self.username = username
+
+  def is_authenticated(self):
+      return True
+
+  def is_active(self):
+      return True
+
+  def is_anonymous(self):
+      return False
+
+  def get_id(self):
+      return self.username
+
+
+
+
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
 from app import shenanigans
-
-port = int(os.environ.get('PORT', 5000)) 
+shenanigans.secret_key = "yoloswag420blazeit"
+port = int(os.environ.get('PORT', 5000))
 shenanigans.run(host='0.0.0.0', port=port, debug=True)
