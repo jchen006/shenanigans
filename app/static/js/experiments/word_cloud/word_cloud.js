da763b94896c15e7ed73f6ce622af4cf9d2c35f6
Jeffrey H. Chen <jeffreyhc@berkeley.edu>
2017-02-08 17:56:03 -0800

    updated word cloud

--- app/static/js/experiments/word_cloud/word_cloud.js
+++ app/static/js/experiments/word_cloud/word_cloud.js
@@ -1,28 +1,21 @@
+var wordCloud = (function() {
+    var word_cloud_renderer = function(frequency_list, width, height) {
 
-
-function word_cloud(num) {
-    d3.json("/api/word_cloud_json/" + num, function(error, wordCloudData) {
-        word_cloud_generator(wordCloudData);
-    });
-}
-
-function word_cloud_generator(frequency_list) {
-
-    var color = d3.scale.linear()
-            .domain([0,1,2,3,4,5,6,10,15,20,100])
+        var color = d3.scale.linear()
+            .domain([0, 1, 2, 3, 4, 5, 6, 10, 15, 20, 100])
             .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
 
-    d3.layout.cloud().size([2000, 1500])
+        d3.layout.cloud().size([width, height])
             .words(frequency_list)
             .rotate(0)
             .fontSize(function(d) { return d.size; })
             .on("end", draw)
             .start();
 
-    function draw(words) {
-        d3.select("body").append("svg")
-                .attr("width", 2000)
-                .attr("height", 1500)
+        function draw(words) {
+            d3.select("body").append("svg")
+                .attr("width", width)
+                .attr("height", height)
                 .attr("class", "wordcloud")
                 .append("g")
                 // without the transform, words words would get cutoff to the left and top, they would
@@ -37,5 +30,14 @@ function word_cloud_generator(frequency_list) {
                     return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
                 })
                 .text(function(d) { return d.text; });
+        }
     }
-}
+
+    return {
+        word_cloud_generator: function(num, width, height) {
+            d3.json("/api/word_cloud_json/" + num, function(error, wordCloudData) {
+                word_cloud_renderer(wordCloudData, width, height);
+            });
+        }
+    }
+})();
