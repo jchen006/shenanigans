ade94e49b39dce5b5a5f8f7ac7f01b964ff228df
Jeffrey H. Chen <jeffreyhc@berkeley.edu>
2017-02-12 01:38:09 -0800

    refactored word cloud

--- app/static/js/experiments/word_cloud/word_cloud.js
+++ app/static/js/experiments/word_cloud/word_cloud.js
@@ -1,43 +1,49 @@
 var wordCloud = (function() {
-    var word_cloud_renderer = function(frequency_list, width, height) {
 
-        var color = d3.scale.linear()
-            .domain([0, 1, 2, 3, 4, 5, 6, 10, 15, 20, 100])
-            .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
+    var num, width, height;
 
-        d3.layout.cloud().size([width, height])
-            .words(frequency_list)
-            .rotate(0)
-            .fontSize(function(d) { return d.size; })
-            .on("end", draw)
-            .start();
+    var generate = function() {
+        d3.json("/api/word_cloud_json/" + num, function(error, frequency_list) {
+            var color = d3.scale.linear()
+                .domain([0, 1, 2, 3, 4, 5, 6, 10, 15, 20, 100])
+                .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
 
-        function draw(words) {
-            d3.select("body").append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .attr("class", "wordcloud")
-                .append("g")
-                // without the transform, words words would get cutoff to the left and top, they would
-                // appear outside of the SVG area
-                .attr("transform", "translate(320,200)")
-                .selectAll("text")
-                .data(words)
-                .enter().append("text")
-                .style("font-size", function(d) { return d.size + "px"; })
-                .style("fill", function(d, i) { return color(i); })
-                .attr("transform", function(d) {
-                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-                })
-                .text(function(d) { return d.text; });
-        }
+            d3.layout.cloud().size([width, height])
+                .words(frequency_list)
+                .rotate(0)
+                .fontSize(function(d) { return d.size; })
+                .on("end", draw)
+                .start();
+
+            function draw(words) {
+                d3.select("body").append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .attr("class", "wordcloud")
+                    .append("g")
+                    // without the transform, words words would get cutoff to the left and top, they would
+                    // appear outside of the SVG area
+                    .attr("transform", "translate(320,200)")
+                    .selectAll("text")
+                    .data(words)
+                    .enter().append("text")
+                    .style("font-size", function(d) { return d.size + "px"; })
+                    .style("fill", function(d, i) { return color(i); })
+                    .attr("transform", function(d) {
+                        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                    })
+                    .text(function(d) { return d.text; });
+            }
+        });
     }
 
     return {
-        word_cloud_generator: function(num, width, height) {
-            d3.json("/api/word_cloud_json/" + num, function(error, wordCloudData) {
-                word_cloud_renderer(wordCloudData, width, height);
-            });
+
+        init: function(config) {
+            num = config.num,
+                width = config.width,
+                height = config.height
+            generate();
         }
     }
 })();
