2d7e04a3d8a013d414b0b6ae5bf85943026b6552
Ryan Hang <rhang@shutterfly.com>
2017-06-21 22:11:17 -0700

    setting up webpack

---
+++ app/static/js/reactComponents/HellWorldComponent.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <div className="helloWorld">
+                <p> Hello World </p>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+
