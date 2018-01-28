ce6d8ab2c4eb3891e2273b0b7728673917ad2d4e
Jeff Chen <>
2018-01-27 23:07:58 -0800

    fixing the libraries

--- app/static/js/components/d3Graphs/WordCloud.jsx
+++ app/static/js/components/d3Graphs/WordCloud.jsx
@@ -60,6 +60,30 @@ class WordCloud extends React.component {
 
 }
 
+// import React from 'react';
+// import { render } from 'react-dom';
+// import WordCloud from 'react-d3-cloud';
+ 
+// const data = [
+//   { text: 'Hey', value: 1000 },
+//   { text: 'lol', value: 200 },
+//   { text: 'first impression', value: 800 },
+//   { text: 'very cool', value: 1000000 },
+//   { text: 'duck', value: 10 },
+// ];
+ 
+// const fontSizeMapper = word => Math.log2(word.value) * 5;
+// const rotate = word => word.value % 360;
+ 
+// render(
+//   <WordCloud
+//     data={data}
+//     fontSizeMapper={fontSizeMapper}
+//     rotate={rotate}
+//   />,
+//   document.getElementById('root')
+// );
+
 WordCloud.propTypes = {
   containerId: PropTypes.string,
   num: PropTypes.number,
